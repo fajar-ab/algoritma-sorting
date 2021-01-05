455cdc62380d38b33db4f28fda86adc520b954d4
Fajar-ab <fajarfanz378@gmail.com>
2021-01-05 21:07:35 +0700

    modifikasi sedikit

--- Shell Sort/Main.java
+++ Shell Sort/Main.java
@@ -19,12 +19,12 @@ public class Main {
 	// method pengurutan secara ascending menggunakan shell sort.
 	private static void shellSort(int[] angka) {  
 
-		for (int i = angka.length / 2; i > 0; i = i / 2)  {    
+		for (int i = angka.length / 2; i > 0; i /= 2) {    
 			for (int j = i; j < angka.length; j++) {    
 				for(int k = j - i; k >= 0; k -= i) {    
-					if (angka[k+i] >= angka[k])    
-						break;    
-					else  {    
+					if (angka[k+i] >= angka[k]) {    
+						break;
+					}else {    
 						int temp = angka[k];    
 						angka[k] = angka[k+i];    
 						angka[k+i] = temp;    
